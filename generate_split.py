39c962789bca8c3674f3f6dd3988bf4c999f24f2
Albert T <tsengalb99@gmail.com>
2020-04-20 06:27:18 +0000

    finish generate_split.py

--- generate_split.py
+++ generate_split.py
@@ -1,14 +1,20 @@
 import numpy as np
 import os
+import copy
+import random
+import json
 
 np.random.seed(2020) # to ensure you always get the same train/test split
 
 data_path = '../data/RedLights2011_Medium'
 gts_path = '../data/hw02_annotations'
 split_path = '../data/hw02_splits'
+preds_path = '../data/hw02_preds'
+
+os.makedirs(split_path, exist_ok=True) # create directory if needed
 os.makedirs(preds_path, exist_ok=True) # create directory if needed
 
-split_test = False # set to True and run when annotations are available
+split_test = True # set to True and run when annotations are available
 
 train_frac = 0.85
 
@@ -19,11 +25,11 @@ file_names = sorted(os.listdir(data_path))
 file_names = [f for f in file_names if '.jpg' in f]
 
 # split file names into train and test
-file_names_train = []
-file_names_test = []
-'''
-Your code below. 
-'''
+
+fn2 = copy.deepcopy(file_names)
+random.shuffle(fn2)
+file_names_train = fn2[:int(train_frac*len(fn2))]
+file_names_test = fn2[int(train_frac*len(fn2)):]
 
 assert (len(file_names_train) + len(file_names_test)) == len(file_names)
 assert len(np.intersect1d(file_names_train,file_names_test)) == 0
@@ -39,9 +45,11 @@ if split_test:
     # annotations
     gts_train = {}
     gts_test = {}
-    '''
-    Your code below. 
-    '''
+
+    for i in file_names_train:
+        gts_train[i] = gts[i]
+    for i in file_names_test:
+        gts_test[i] = gts[i]
     
     with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
         json.dump(gts_train,f)
